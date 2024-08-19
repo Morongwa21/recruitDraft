ac16d4b892422a6b7b39ef4d19fc932fd036f2ff
Morongwa21 <angelmalatji5@gmail.com>
2024-08-19 11:11:02 +0200

    updates

---
+++ src/Notifications.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+import './components/Notifications.css'; // Add your styles here
+
+const Notifications = () => {
+    const [notifications, setNotifications] = useState([]);
+
+    useEffect(() => {
+        fetchNotifications();
+    }, []);
+
+    const fetchNotifications = async () => {
+        try {
+            const response = await axios.get('https://recruitment-portal-rl5g.onrender.com/notifications');//To get the endpoint from Tiyani
+            setNotifications(response.data);
+        } catch (error) {
+            console.error('Error fetching notifications:', error.message);
+        }
+    };
+
+    return (
+        <div className="notifications-page">
+            <h2>Notifications</h2>
+            <ul className="notifications-list">
+                {notifications.map((notification, index) => (
+                    <li key={index} className="notification-item">
+                        <span>{notification.message}</span>
+                        <span className="notification-time">{new Date(notification.createdAt).toLocaleString()}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Notifications;
